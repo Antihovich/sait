0099f21cf5c479b6fe12138231a2ef29a41df411
yk <yk@gmail.com>
2018-10-24 18:00:49 +0300

    mycommit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+  <html lang="ru">
+    <head>
+	 <meta charset="utf-8">
+	 <meta name="description" content="1,2,3 предложения">
+	 <meta name="keywords" content="выражения через ,">
+	 <meta name="author" content="Antihovich">
+	 <title>Название сайта</title>
+	 <link type="text/css" rel="stylesheet" href="media/bootstrap/css/bootstrap.min.css">
+	 <link type="text/css" rel="stylesheet" href="media/css/index.css" />
+	 
+	</head>
+	<body>
+	 <div id="header">
+	  <h1 id="logotext">Название сайта </h1>
+	  <img id="logo" src="media/img/logo.png"/>
+	  </div>
+	  <nav class="topmenu">
+	  <a href="#">Главная</a>
+	  <a href="#">Меню</a>
+	  <a href="#">О нас</a>
+	  <a href="#">Услуги</a>
+	   <a href="#">Контакты</a>
+	   </nav>
+	   <div class="col-md-2">
+	   <a href="#" class="btn btn-default btn-block">Текст</a>
+	   <a href="#" class="btn btn-default btn-block">Текст</a>
+	   <a href="#" class="btn btn-default btn-block">Текст</a>
+	 
+	   </div>
+	   <div class="col-md-8">
+	    <h2>Добро пожаловать на сайт</h2>
+		<div class="maintext">
+		   <p class="lead">Bootstrap uses <a href="http://gruntjs.com">Grunt</a> for its build system, with convenient methods for working with the framework. It's how we compile our code, run tests, and more.</p> <h2 id="grunt-installing"><a class="anchorjs-link " href="#grunt-installing" aria-label="Anchor link for: grunt installing" data-anchorjs-icon="" style="font-family: anchorjs-icons; font-style: normal; font-variant: normal; font-weight: normal; line-height: inherit; position: absolute; margin-left: -1em; padding-right: 0.5em;"></a>Installing Grunt</h2> <p>To install Grunt, you must <strong>first <a href="https://nodejs.org/download/">download and install node.js</a></strong> (which includes npm). npm stands for <a href="https://www.npmjs.com/">node packaged modules</a> and is a way to manage development dependencies through node.js.</p> Then, from the command line: <ol> <li>Install <code>grunt-cli</code> globally with <code>npm install -g grunt-cli</code>.</li> <li>Navigate to the root <code>/bootstrap/</code> directory, then run <code>npm install</code>. npm will look at the <a href="https://github.com/twbs/bootstrap/blob/master/package.json"><code>package.json</code></a> file and automatically install the necessary local dependencies listed there.</li> </ol> <p>When completed, you'll be able to run the various Grunt commands provided from the command line.</p> <h2 id="grunt-commands">Available Grunt commands</h2> <h3 id="grunt-dist-just-compile-css-and-javascript"><a class="anchorjs-link " href="#grunt-dist-just-compile-css-and-javascript" aria-label="Anchor link for: grunt dist just compile css and javascript" data-anchorjs-icon="" style="font-family: anchorjs-icons; font-style: normal; font-variant: normal; font-weight: normal; line-height: inherit; position: absolute; margin-left: -1em; padding-right: 0.5em;"></a><code>grunt dist</code> (Just compile CSS and JavaScript)</h3> <p>Regenerates the <code>/dist/</code> directory with compiled and minified CSS and JavaScript files. As a Bootstrap user, this is normally the command you want.</p> <h3 id="grunt-watch-watch"><a class="anchorjs-link " href="#grunt-watch-watch" aria-label="Anchor link for: grunt watch watch" data-anchorjs-icon="" style="font-family: anchorjs-icons; font-style: normal; font-variant: normal; font-weight: normal; line-height: inherit; position: absolute; margin-left: -1em; padding-right: 0.5em;"></a><code>grunt watch</code> (Watch)</h3> <p>Watches the Less source files and automatically recompiles them to CSS whenever you save a change.</p> <h3 id="grunt-test-run-tests"><a class="anchorjs-link " href="#grunt-test-run-tests" aria-label="Anchor link for: grunt test run tests" data-anchorjs-icon="" style="font-family: anchorjs-icons; font-style: normal; font-variant: normal; font-weight: normal; line-height: inherit; position: absolute; margin-left: -1em; padding-right: 0.5em;"></a><code>grunt test</code> (Run tests)</h3> <p>Runs <a href="http://jshint.com">JSHint</a> and runs the <a href="http://qunitjs.com">QUnit</a> tests headlessly in <a href="http://phantomjs.org">PhantomJS</a>.</p> <h3 id="grunt-docs-build-test-the-docs-assets"><a class="anchorjs-link " href="#grunt-docs-build-test-the-docs-assets" aria-label="Anchor link for: grunt docs build test the docs assets" data-anchorjs-icon="" style="font-family: anchorjs-icons; font-style: normal; font-variant: normal; font-weight: normal; line-height: inherit; position: absolute; margin-left: -1em; padding-right: 0.5em;"></a><code>grunt docs</code> (Build &amp; test the docs assets)</h3> <p>Builds and tests CSS, JavaScript, and other assets which are used when running the documentation locally via <code>bundle exec jekyll serve</code>.</p> <h3 id="grunt-build-absolutely-everything-and-run-tests"><a class="anchorjs-link " href="#grunt-build-absolutely-everything-and-run-tests" aria-label="Anchor link for: grunt build absolutely everything and run tests" data-anchorjs-icon="" style="font-family: anchorjs-icons; font-style: normal; font-variant: normal; font-weight: normal; line-height: inherit; position: absolute; margin-left: -1em; padding-right: 0.5em;"></a><code>grunt</code> (Build absolutely everything and run tests)</h3> <p>Compiles and minifies CSS and JavaScript, builds the documentation website, runs the HTML5 validator against the docs, regenerates the Customizer assets, and more. Requires <a href="http://jekyllrb.com/docs/installation/">Jekyll</a>. Usually only necessary if you're hacking on Bootstrap itself.</p> <h2 id="grunt-troubleshooting"><a class="anchorjs-link " href="#grunt-troubleshooting" aria-label="Anchor link for: grunt troubleshooting" data-anchorjs-icon="" style="font-family: anchorjs-icons; font-style: normal; font-variant: normal; font-weight: normal; line-height: inherit; position: absolute; margin-left: -1em; padding-right: 0.5em;"></a>Troubleshooting</h2> <p>Should you encounter problems with installing dependencies or running Grunt commands, first delete the <code>/node_modules/</code> directory generated by npm. Then, rerun <code>npm install</code>.</p>   
+		
+	   
+	   </div>
+	   </div>	  
+	   <div class="col-md-2">
+	   <!--pogoda.by-->
+<!--Информер распространяется свободно и на безвозмездной основе. Однако, в случае изменения кода информера (в частности, удаления ссылок), POGODA.BY снимает с себя ответственность за конечный результат.-->
+
+<table width="100%" height="180" style="background-color:#f2f2f2; border: #cccccc 1px solid; font-family:Tahoma; font-size:12px; color:#000000;" cellpadding="2" cellspacing="0">
+<tr><td><a href="http://6.pogoda.by/26850" style="font-family:Tahoma; font-size:12px; color:#003399;" title="Погода Минск на 6 дней - Гидрометцентр РБ" target="_blank">Погода Минск</a>
+</td></tr>
+<tr><td>
+
+<table width=100% height=100% style="background-color:#f2f2f2; font-family:Tahoma; font-size:12px; color:#000000;" cellpadding="0" cellspacing="0">
+<tr><td>
+<script type="text/javascript" charset="windows-1251" src="http://pogoda.by/meteoinformer/js/26850_3.js"></script>
+</td></tr>
+</table>
+
+</td></tr>
+
+<tr><td align="right">Информация сайта <a href="http://www.pogoda.by" target="_blank" style="font-family:Tahoma; font-size:12px; color:#003399;">pogoda.by</a>
+</td></tr>
+</table>
+	   </div>
+	   <br style="clear:both" />
+	   <div id="footer">
+	   2018 &copy; Antihovich.ru
+	   
+	   
+	</body>
+  </html>	
+	
