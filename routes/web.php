a60783f0c5ecd05595b54807939efad439f5305d
yk <yk@gmail.com>
2018-10-26 18:16:06 +0300

    katalog

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,6 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::post('/home', 'HomeController@postIndex');
 Route::get('feedback', 'FeedbackController@getIndex');
+Route::get('catalog/{id}', 'CatalogController@getIndex');
 //всегда последний
 Route::get('{url}', 'StaticController@getIndex');
